f126714ca884bf2cb8c4847cdc8005ede01b7e3e
Eric Matlock <ermatlock@gmail.com>
2021-11-19 11:46:13 -0500

    Complete unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,16 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
+  }
+  isWhite() {
+    if (this.color != 'white') {
+      return false;
+    }
+  }
+  says(statement) {
+    return `**;* ${statement} *;**`
   }
 }
 
-module.exports = Unicorn;
+module.exports = Unicorn
