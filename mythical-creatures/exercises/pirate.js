637bb0e2a2f2e49c3b05b67cecf24d3e2ca8112f
Eric Matlock <ermatlock@gmail.com>
2021-11-27 14:27:56 -0500

    Complete Pirate

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,27 @@
+class Pirate {
+  constructor(name, job) {
+    this.name = name;
+    this.job = job || 'scallywag';
+    this.cursed = false;
+    this.booty = 0;
+  };
+  robShip() {
+    if (this.booty === 500) {
+      this.cursed = true;
+      return `ARG! I\'ve been cursed!`;
+    }
+    this.booty += 100;
+    return `YAARRR!`;
+  };
+  liftCurse() {
+    if (!this.cursed) {
+      return `You don\'t need to lift a curse!`;
+    } else {
+        this.booty = 200;
+        this.cursed = false;
+        return `Your curse has been lifted!`;
+    };
+  };
+};
+
+module.exports = Pirate;
