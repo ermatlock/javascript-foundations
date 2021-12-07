2f474c5321ed281993e8f427022e3477b54c4f80
Eric Matlock <ermatlock@gmail.com>
2021-12-06 23:54:03 -0500

    Complete centaur

---
+++ mythical-creatures/exercises/centaur.js
@@ -0,0 +1,52 @@
+class Centaur {
+  constructor(centaur) {
+    this.name = centaur.name;
+    this.breed = centaur.type;
+    this.cranky = false;
+    this.standing = true;
+    this.layingDown = false;
+    this.crankyCount = 0
+  }
+  shootBow() {
+    this.crankyCount++
+    if (this.crankyCount >= 3 || !this.standing) {
+      this.cranky = true;
+      return 'NO!';
+    }
+    return 'Twang!!!';
+
+  }
+  run() {
+    this.crankyCount++
+    if (this.crankyCount >= 3 || !this.standing) {
+      this.cranky = true;
+      return 'NO!';
+    }
+    return 'Clop clop clop clop!!!';
+  }
+  sleep() {
+    if (this.standing) {
+      return 'NO!';
+    }
+    this.cranky = false;
+    this.crankyCount = 0;
+    return 'ZZZZ';
+  }
+  layDown() {
+    this.standing = false;
+    this.layingDown = true;
+  }
+  standUp() {
+    this.standing = true;
+    this.layingDown = false;
+  }
+  drinkPotion() {
+    if (!this.standing) {
+      return 'Not while I\'m laying down!';
+    }
+    this.cranky = false;
+    this.crankyCount = 0;
+  }
+};
+
+module.exports = Centaur;
