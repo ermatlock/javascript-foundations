74eb487ff3f4236e86e80cf0614c6e5d60ab5a80
Eric Matlock <ermatlock@gmail.com>
2021-12-16 16:17:05 -0500

    Complete ogre

---
+++ mythical-creatures/exercises/human.js
@@ -0,0 +1,19 @@
+// var Ogre = require('./ogre.js');
+
+class Human {
+  constructor(name) {
+    this.name = name;
+    this.encounterCounter = 0;
+    this.knockedOut = false;
+  }
+
+  noticesOgre() {
+    if (this.encounterCounter === 3 || this.encounterCounter === 6) {
+      return true;
+    }
+    return false;
+  }
+
+}
+
+module.exports = Human;
---
+++ mythical-creatures/exercises/ogre.js
@@ -0,0 +1,31 @@
+// var Human = require('./human.js');
+
+class Ogre {
+  constructor(ogre) {
+    this.name = ogre.name;
+    this.home = ogre.abode || 'Swamp';
+    this.swings = 0;
+
+  }
+
+  encounter(human) {
+    human.encounterCounter++;
+    if (human.encounterCounter === 3 || human.encounterCounter === 6) {
+      this.swingAt(human);
+    }
+  }
+
+  swingAt(human) {
+    this.swings++
+    if (this.swings === 2) {
+      human.knockedOut = true;
+    }
+  }
+
+  apologize(human) {
+    human.knockedOut = false;
+  }
+
+}
+
+module.exports = Ogre;
