0bde10b3626d8bbd277e766dc6030213f780e96e
Eric Matlock <ermatlock@gmail.com>
2022-08-15 15:04:08 -0400

    Complete golf course exercises

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,5 +1,45 @@
 class Golfer {
+  constructor(golfer) {
+    this.name = golfer.name;
+    this.handicap = golfer.handicap;
+    this.frustration = 0;
+    this.confidence = 0;
+  }
 
+  calculateAvg(par) {
+    return `I usually shoot a ${par + this.handicap} on average.`
+  }
+
+  playRound(golfCourse) {
+    if (golfCourse.difficulty === 'hard') {
+      this.frustration += 500;
+    } else if (golfCourse.difficulty === 'moderate') {
+      this.frustration += 100;
+    }
+  }
+
+  hitTheRange() {
+    this.confidence += 10;
+  }
+
+  marvel(golfCourse) {
+    return `I love the ${golfCourse.features[0]} and ${golfCourse.features[1]} on this course!`;
+  }
+
+  whatYaShoot(score) {
+    if (score >= 1) {
+      this.frustration += 20;
+      return 'Doh!'
+    } else if (score === 0) {
+      this.frustration -= 10;
+      return 'Booyah!'
+    } else if (score < 0) {
+      this.frustration = 0;
+      return 'I AM THE GREATEST GOLFER ALIVE!'
+    }
+
+    return 'I AM THE GREATEST GOLFER ALIVE!'
+  }
 
 }
 
