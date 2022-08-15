90cf633b382e113696f63205d72d772308264ba1
Eric Matlock <ermatlock@gmail.com>
2022-08-15 15:04:17 -0400

    Complete exercises

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,4 +1,26 @@
 class LunchBox {
+	constructor(box) {
+		this.owner = box.owner;
+		this.material = box.madeOf;
+		this.shape = box.shape;
+		this.color = box.color;
+		this.snacks = [];
+	}
+
+	acquireSnack(snack) {
+		this.snacks.push(snack);
+		snack.isInLunchBox = true;
+	}
+
+	findHealthySnacks() {
+		var healthySnacks = [];
+		for (var i = 0; i < this.snacks.length; i++) {
+			if(this.snacks[i].checkForHealthy()) {
+				healthySnacks.push(this.snacks[i].type);
+			}
+		}
+		return healthySnacks;
+	}
 
 }
 
